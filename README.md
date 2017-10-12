00e3725f4fbd440c797c896483db537e2213cc54
Marius Claret <marius.claret@gmail.com>
2017-10-12 18:07:01 +0200

    Initialization

---
+++ README.md
@@ -0,0 +1,27 @@
+# Ripple Effect
+
+Get a beautiful Material Design ripple effect in pure JavaScript for your website in less than 1 minute. The script is not blocking rendering on the first paint, because the effect only gets fired on `onpointerdown` and `onpointerup` events on the `document`.
+
+## Getting Started
+
+Import the **ripple.js** file in your project:
+```html
+<script src="ripple.js"></script>
+```
+
+Add **ripple class** to your element:
+```html
+<button class="ripple">Button with ripple effect</button>
+```
+
+Done!
+
+## Demo
+
+Works only in browsers with pointer events support.
+
+https://htmlpreview.github.io/?https://github.com/mariusclaret/ripple/blob/master/demo.html
+
+## Author
+
+Marius Claret (@mariusclaret || https://twitter.com/mariusclaret)
---
+++ ripple.js
@@ -0,0 +1,62 @@
+let current;
+
+document.onpointerdown = function (event) {
+    if (current) {
+        const remove = current;
+        current = null;
+        setTimeout(function () {
+            if (remove.parentNode) remove.parentNode.removeChild(remove);
+        }, 800);
+    }
+
+    let target = event.target;
+    while (target && target.classList && !target.classList.contains("ripple")) target = target.parentNode;
+    if (!target || !target.classList || !target.classList.contains("ripple")) return;
+
+    const x = event.pageX - target.getBoundingClientRect().left;
+    const y = event.pageY - target.getBoundingClientRect().top;
+    const maxW = Math.max(x, target.offsetWidth - x);
+    const maxH = Math.max(y, target.offsetHeight - y);
+    const size = Math.sqrt(maxW * maxW + maxH * maxH);
+
+    const parent = document.createElement("div");
+    parent.style.position = "absolute";
+    parent.style.top = "0";
+    parent.style.right = "0";
+    parent.style.bottom = "0";
+    parent.style.left = "0";
+    parent.style.overflow = "hidden";
+    parent.style.borderRadius = "inherit";
+    parent.style.transform = "perspective(0)";
+    target.appendChild(parent);
+
+    const effect = document.createElement("div");
+    effect.style.position = "absolute";
+    effect.style.top = (y - size) + "px";
+    effect.style.left = (x - size) + "px";
+    effect.style.height = size * 2 + "px";
+    effect.style.width = size * 2 + "px";
+    effect.style.background = "rgba(0, 0, 0, 0.04)";
+    effect.style.borderRadius = "50%";
+    effect.style.transform = "scale(0)";
+    effect.style.transition = "opacity 640ms, transform 640ms";
+    parent.appendChild(effect);
+
+    current = parent;
+
+    const timeout = setTimeout(function () {
+        effect.style.transform = "scale(1)";
+    }, 16);
+
+    document.onpointerup = document.onpointercancel = function () {
+        document.onpointerup = document.onpointercancel = document.onpointermove = null;
+        current.firstChild.style.opacity = "0";
+    };
+
+    document.onpointermove = function (move) {
+        if (event.x - move.x > 4 || event.x - move.x < -4 || event.y - move.y > 4 || event.y - move.y < -4) {
+            clearTimeout(timeout);
+            document.onpointercancel();
+        }
+    };
+};
